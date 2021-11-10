b9b23130f1e710c1c17234ccc73cd2bd8003cf04
Fernando Mette <metteinfor@gmail.com>
2021-11-10 13:46:59 -0300

    trazendo as funções do script para o pthon como comentario

--- main.py
+++ main.py
@@ -1,2 +1,71 @@
 variavelqualquer = 'Ola mundo'
 print(variavelqualquer)
+
+
+'''
+        function somar() {
+
+                var numerodigitado = parseFloat(document.getElementById("caixanumerodigitado").value)
+                
+                var resultado = parseFloat(document.querySelector("#resultado").innerText)
+
+                resultado = resultado + numerodigitado
+           
+                document.querySelector("#resultado").innerText = resultado
+
+                document.getElementById("caixanumerodigitado").value = 0
+
+        }
+        
+        function diminuir() {
+
+                var numerodigitado = parseFloat(document.getElementById("caixanumerodigitado").value)
+                
+                var resultado = parseFloat(document.querySelector("#resultado").innerText)
+    
+                resultado = resultado - numerodigitado
+               
+                document.querySelector("#resultado").innerText = resultado
+    
+                document.getElementById("caixanumerodigitado").value = 0
+
+        }
+
+        function multiplicar() {
+
+                var numerodigitado = parseFloat(document.getElementById("caixanumerodigitado").value)
+                
+                var resultado = parseFloat(document.querySelector("#resultado").innerText)
+    
+                resultado = resultado * numerodigitado
+               
+                document.querySelector("#resultado").innerText = resultado
+    
+                document.getElementById("caixanumerodigitado").value = 0
+
+        }
+
+        function dividir() {
+
+                var numerodigitado = parseFloat(document.getElementById("caixanumerodigitado").value)
+                
+                var resultado = parseFloat(document.querySelector("#resultado").innerText)
+    
+                resultado = resultado / numerodigitado
+               
+                document.querySelector("#resultado").innerText = resultado
+    
+                document.getElementById("caixanumerodigitado").value = 0
+
+        }
+
+        function zerarresultado() {
+
+            document.querySelector("#resultado").innerText = 0
+            
+            document.getElementById("caixanumerodigitado").value = 0
+
+        }
+        
+        
+'''
